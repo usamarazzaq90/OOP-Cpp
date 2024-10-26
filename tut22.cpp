82314b06afa6e836cbf07bcd94d008a31433881f
usamarazzaq90 <usamarazzaq9014@gmail.com>
2024-10-26 07:13:53 +0500

    updated

--- tut22.cpp
+++ tut22.cpp
@@ -2,19 +2,18 @@
 
 //  C++ --> initially called --> C with classes by stroustroup
 //	class --> extension of structures (in C)
-//  Structures had limits: 
-//         - Members are public 
+//  Structures had limits:
+//         - Members are public
 //         - No methods
 //  classes --> structures + more
 //  classes --> can have methods and properties
-//  classes --> can make few members as priate and few as public
+//  classes --> can make few members as private and few as public
 //	structures in C++ are typedef
 //	you can declare objects along with class declaration like:
-	/*	class Employee{
-			//class definition
-		} usama, usman, sufyan; */
+/*	class Employee{
+		//class definition
+	} usama, usman, sufyan; */
 //	usama.salary = 8 makes no sense if salary is private
-
 
 //	Nesting of member functions
 
@@ -22,55 +21,67 @@
 #include <string>
 using namespace std;
 
-class binary{
-//	private:     (By default, it is private)
-		string s;
-		void chk_bin(void);
-	public:
-		void read(void);
-		void ones_compliment(void);
-		void display(void);
+class binary
+{
+	//	private:     (By default, it is private)
+	string s;
+	void chk_bin(void);
+
+public:
+	void read(void);
+	void ones_compliment(void);
+	void display(void);
 };
 
-void binary::read(){
-	cout<<"Enter a binary number "<<endl;
-	cin>>s;
+void binary::read()
+{
+	cout << "Enter a binary number " << endl;
+	cin >> s;
 }
 
-void binary::chk_bin(){
-	for(int i=0; i<s.length(); i++){
-		if(s.at(i)!='0' && s.at(i)!='1'){
-			cout<<"Incorrect binary format"<<endl;
-			exit(0);   //Exit the program
+void binary::chk_bin()
+{
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s.at(i) != '0' && s.at(i) != '1')
+		{
+			cout << "Incorrect binary format" << endl;
+			exit(0); // Exit the program
 		}
 	}
 }
 
-void binary::ones_compliment(){
-	for(int i=0; i<s.length(); i++){
-		if(s.at(i)=='0') s.at(i)='1';
-		else if(s.at(i)=='1') s.at(i)='0';
+void binary::ones_compliment()
+{
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s.at(i) == '0')
+			s.at(i) = '1';
+		else if (s.at(i) == '1')
+			s.at(i) = '0';
 	}
 }
 
-void binary::display(){
-	chk_bin();      //Nesting
-	cout<<"Displaying your binary number "<<endl;
-	for(int i=0; i<s.length(); i++){
-		cout<<s.at(i);
+void binary::display()
+{
+	chk_bin(); // Nesting
+	cout << "Displaying your binary number : ";
+	for (int i = 0; i < s.length(); i++)
+	{
+		cout << s.at(i);
 	}
-	cout<<endl;
+	cout << endl;
 }
 
-
-int main(){
+int main()
+{
 	binary b;
 	b.read();
-//	b.chk_bin();    It is private
+	//	b.chk_bin();    It is private
 	b.display();
 	b.ones_compliment();
-	cout<<"After taking 1's compliment "<<endl;
+	cout << "After taking 1's compliment" << endl;
 	b.display();
-	
+
 	return 0;
 }
