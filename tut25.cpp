7038e86fcc7f5e575da984b2b4d937bf149e1e3c
usamarazzaq90 <usamarazzaq9014@gmail.com>
2024-10-27 07:05:31 +0500

    dailyy commit

--- tut25.cpp
+++ tut25.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
 using namespace std;
 
-class Employee{
+class Employee
+{
 	int id;
 	int salary;
-	public:
-		void setId(){
-			salary=122;
-			cout<<"Enter ID of employee: ";
-			cin>>id;
-		}
-		void getId(){
-			cout<<"The ID of this employee is "<<id<<endl;
-		}
+
+public:
+	void setId()
+	{
+		salary = 122;
+		cout << "Enter ID of employee: ";
+		cin >> id;
+	}
+	void getId()
+	{
+		cout << "The ID of this employee is " << id << endl;
+	}
 };
-int main(){
-//	Employee usama,bilal,usman,sufyan;
-//	usama.setId();
-//	usama.getId();
+int main()
+{
+	//	Employee usama,bilal,usman,sufyan;
+	//	usama.setId();
+	//	usama.getId();
 
 	Employee fb[4];
-	for(int i=0;i<4;i++){
-		fb[i].setId();	
+	for (int i = 0; i < 4; i++)
+	{
+		fb[i].setId();
 		fb[i].getId();
 	}
-	
+	cout << "Happy Ending!" << endl;
 	return 0;
 }
